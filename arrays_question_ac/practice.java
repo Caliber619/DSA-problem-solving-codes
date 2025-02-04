ba56e137435b161060748d2e64007df1008015f3
Caliber619 <caliber619pubg@gmail.com>
2025-02-04 23:42:29 +0530

    new files added

---
+++ arrays_question_ac/practice.java
@@ -0,0 +1,13 @@
+public class practice {
+    public static void main(String[] args) {
+        int arr[] = {2,6,2,9,12,1,16};
+        int sortedArray[] = {2,4,5,18,20};
+        
+        
+    }
+
+    static void func(int []arr, int n){
+        System.out.println("hey");
+    }
+
+}
