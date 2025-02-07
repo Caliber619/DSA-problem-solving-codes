6e2dc948c381790ff62245aa8f924f1e4dd7dc0a
Caliber619 <caliber619pubg@gmail.com>
2025-02-07 10:27:56 +0530

    kadanes added

---
+++ question_solving_tuf/arrays_medium/kadanes.java
@@ -0,0 +1,29 @@
+public class kadanes {
+    static void func(int [] arr){
+        //lets try to print all the sub-arrays
+        // int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+        int n = arr.length;
+        int maxCount = 0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int subArrayCount = 0;
+                for(int k=i;k<=j;k++){
+                    // System.out.print(arr[k]+" ");
+                    subArrayCount += arr[k];
+                }
+                if(subArrayCount > maxCount){
+                   maxCount = subArrayCount; 
+                }
+                // System.out.println();
+            }
+            // System.out.println();
+        }
+        System.out.println(maxCount);
+        
+    }
+    public static void main(String[] args) {
+        int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+        func(arr);
+    }
+    
+}
