7a4f423d14c2c5cc1bcd6ef49dfa83a74f4cd5c1
Caliber619 <caliber619pubg@gmail.com>
2025-03-02 01:10:08 +0530

    updates

---
+++ imp75/_01Array/TwoSum.java
@@ -0,0 +1,15 @@
+package imp75._01Array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    static void solution(int nums[], int target){
+        for(int i=0; i<nums.length; i++){
+            // 
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
