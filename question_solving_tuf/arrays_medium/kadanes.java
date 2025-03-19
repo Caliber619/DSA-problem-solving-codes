2b2e70c2e83de6a87ddbc40ec40ee58002ac9d43
Caliber619 <caliber619pubg@gmail.com>
2025-03-19 20:17:01 +0530

    added Stack type1 questions

--- question_solving_tuf/arrays_medium/kadanes.java
+++ question_solving_tuf/arrays_medium/kadanes.java
@@ -27,7 +27,7 @@ public class kadanes {
     //keep in mind that the suba
     public static void main(String[] args) {
         int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
-        // func(arr);
+        func(arr);
         // func2(arr);
     }
     
---
+++ stack_questions_ac/_08ReverseString.java
@@ -0,0 +1,38 @@
+package stack_questions_ac;
+import java.util.*;
+
+public class _08ReverseString {
+    public static String reverse(String s){
+
+        int iterator = 0;
+
+        Stack <Character> list = new Stack<>();
+        //converting string to a Stack
+        while(iterator<s.length()){
+            list.push(s.charAt(iterator));
+            iterator++;
+        }
+
+        //StringBuilder to store resultant string
+
+        StringBuilder sb = new StringBuilder();
+
+        while(!list.isEmpty()){
+            sb.append(list.peek());
+            list.pop();
+        }
+
+        return sb.toString();
+
+
+    }
+
+    public static void main(String args[]){
+
+        String s = "abc";
+        
+        String result = reverse(s);
+        System.out.println(result);
+    }
+      
+}
