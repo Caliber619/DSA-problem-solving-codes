5d0c39346fae15967f4815f227de4328921c5d4c
Caliber619 <caliber619pubg@gmail.com>
2025-03-22 06:20:33 +0530

    practice stockspan

--- stack_questions_ac/_06StockSpan.java
+++ stack_questions_ac/_06StockSpan.java
@@ -7,7 +7,8 @@ public class _06StockSpan {
         //array to store stock span
         int span [] = new int[stock.length];
 
-        stockSpan(stock,span);
+        // stockSpan(stock,span);
+        stockSpan2(stock,span);
 
         // for(int i =0; i<span.length; i++){
         //     System.out.println(span[i]);
@@ -15,6 +16,30 @@ public class _06StockSpan {
         System.out.println(Arrays.toString(span));
     }
 
+    public static void stockSpan2(int []stock, int[] span){
+        // stack indexes rkhne ke liye
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0); //because phle element ka index stack me dalna padega
+
+        for(int i=1;i<stock.length;i++){
+            //stock ek ek krke check krege
+            // int stock [] = {100,80,60,70,60,85,100};
+            int current = stock[i];
+
+            while(!s.isEmpty() && current >= stock[s.peek()]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                span[i] = i+1;
+            }else{
+                int prevHigh = s.peek();
+                span[i] = i - prevHigh; 
+            }
+            s.push(i);
+        }
+
+    }
     public static void stockSpan(int []stock, int []span){
         
         //this stack is storing the indexes of the stock prices
