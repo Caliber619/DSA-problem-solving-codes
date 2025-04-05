be2b42f25fc40eff50283f08de9a56f445ec5b45
Caliber619 <caliber619pubg@gmail.com>
2025-04-06 03:59:43 +0530

    binary part 1 completed

--- binaryTree_questions_ac/_01Preorder.java
+++ binaryTree_questions_ac/_01Preorder.java
@@ -89,6 +89,41 @@ public class _01Preorder {
             }
         }
     }
+
+    public static int height(Node root){
+        if(root == null){
+            return 0;
+        }
+        
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        
+        return Math.max(leftHeight,rightHeight) + 1 ;
+    } 
+
+    public static int countNodes(Node root){
+        if(root == null){
+            return 0;
+        }
+
+        int leftCount = countNodes(root.left);
+        int rightCount = countNodes(root.right);
+
+        return leftCount+rightCount + 1;
+
+    }
+
+    public static int sumOfNodes(Node root){
+        if(root == null){
+            return 0;
+        }
+
+        int leftData = sumOfNodes(root.left);
+        int rightData = sumOfNodes(root.right);
+
+        return leftData + rightData + root.data;
+
+    }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1,-1};
         BinaryTree tree = new BinaryTree();
@@ -102,5 +137,10 @@ public class _01Preorder {
         postOrder(root);
         System.out.println();
         levelOrder(root);
+
+        System.out.println();
+        System.out.println(countNodes(root));
+
+        System.out.println(sumOfNodes(root));
     }
 }
