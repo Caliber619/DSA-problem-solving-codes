dc8b8b3ac01f2626253bee2b36ea82b8ac758111
Caliber619 <caliber619pubg@gmail.com>
2025-04-06 04:02:39 +0530

    binary part2 started

---
+++ binaryTree_questions_ac/_02Diameter.java
@@ -0,0 +1,7 @@
+package binaryTree_questions_ac;
+
+public class _02Diameter {
+    public static void main(String[] args) {
+        // diameter is the number of nodes in the longest path between 2 leaves
+    }
+}
